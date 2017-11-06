ff03cf9a77e59286696594774660a562ff9d123b
haizhi.hua <haizhi.hua@kaiqigu.com>
2017-11-06 14:44:36 +0800

    初始化工程

---
+++ SecretAlbum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretAlbum
+//
+//  Created by 华海智 on 2017/11/6.
+//  Copyright © 2017年 海志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
